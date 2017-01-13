fb54a513796961fb2dd28f31b1b502dadabd26a5
Afriportal <Afriportal@users.noreply.github.com>
2017-01-13 17:07:43 +0100

    Updated conn.php
    Changed password varchar(30) to varchar(50)

--- conn.php
+++ conn.php
@@ -30,7 +30,7 @@ $query = "CREATE TABLE users (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(30) NOT NULL,
 	email VARCHAR(50),
-	password VARCHAR(30) NOT NULL,
+	password VARCHAR(50) NOT NULL,
 	reg_date TIMESTAMP
 )";
 
